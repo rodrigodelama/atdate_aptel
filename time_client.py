1b781a90339737e8ae7f103c0bae952feb535d7f
Rodrigo De Lama <rodrigo.delama@icloud.com>
2022-09-26 21:23:40 +0200

    code clean up
    cleaned up code and comments
    
    deleted obsolete version
    
    started adding and formating the proyect outline

--- time_client.py
+++ time_client.py
@@ -12,67 +12,76 @@ Time since UNIX existance 2208988800 (1970)
 
 In plain-text:
 The server listens for a connection on port 37. When the connection
-is established, the client must send an empty TCP packet to which
+is established, the client must send an empty packet to which
 the server returns a 32-bit time value (data) and closes the
 connection.
 '''
 
 import sys # For commandline args
-from socket import socket, getaddrinfo, SOCK_STREAM, AF_INET, gaierror
-import time
-import struct # To isolate our desired info from the packet
+from socket import socket, getaddrinfo, SOCK_STREAM, AF_INET, gaierror #SOCK_STREAM is TCP, AF_INET is Addr. Fam. IPv4
+from time import gmtime # The time.pyi library has this funtion to format secconds
+import struct # To isolate our desired info from the packet with unpack()
 
+# Define a buffer size for the 32 bit BIN number we will recieve
+# 4 bytes * 8 bits/byte = 32 bits
+BUFFSIZE = 4
 # Substract 2 hours, to get (CEST +2 hours)
 time_delta = 2208988800 - 3600*2 # epoch time - 2 hours
 
 
-# Define a buffer size for the 32 bit BIN number we will recieve
-BUFFSIZE = 4 # 4 bytes * 8 bits/byte = 32 bits
+def get_current_time():
+    # Filter input args or instruct usage
+    if len(sys.argv) == 2:
+        target = sys.argv[1]
+    else:
+        print("Input the IP address of the desired time server")
+        print("Usage: %s <IP Address>\n" (sys.argv[0]))
+        sys.exit(1)
 
-if len(sys.argv) == 2:
-    target = sys.argv[1]
-else:
-    print("Input the IP address of the desired time server\n")
-    print("Usage: %s <IP Address>\n" (sys.argv[0]))
-    sys.exit(1)
+    print("Attempting to connect to:", target)
+    # Get address info in a "2D" tuple
+    # Index [0] is the IP address of the desired hostname
+    # Index [-1] is the definition of the Port No. where we will open our socket
+    server = getaddrinfo(target, 37)[0][-1] # 2D 
 
-print("Attempting to connect to:", target)
+    # Test print input address data:
+    # (Format is: ('IP', PORT_NO) )
+    # print(server)
 
-# Get address info
-server_address = getaddrinfo(target, 37)[0][-1] # 2D Tuple, we select the two indexes we want (Ip, port)
-#print(server_address) # Test print
+    sockett = socket(AF_INET, SOCK_STREAM)
+    try:
+        sockett.connect(server)
+    except socket.gaierror:
+        print("Error")
 
-# Open a socket at the inputted IP address, Port 37
-sockett = socket(AF_INET, SOCK_STREAM)
+    # If the connection succeeds, send the empty TCP message
+    sockett.send(bytes(0))
 
-try:
-    sockett.connect(server_address) # 37 is the dedicated TIME port
-except socket.gaierror:
-    print("Error")
+    # Revieve the 4byte (32bit) current time data
+    recv_data = sockett.recv(BUFFSIZE)
+    # print(recv_data) # WHAT ENCODING IS THIS ??
 
-# If the connection succeeds, send the empty TCP message
-sockett.send(bytes(0))
+    sockett.close() # Close the socket
 
-recv_data = sockett.recv(BUFFSIZE) # An int that represents a number of bytes
+    time_since_1970 = struct.unpack("!I", recv_data)[0] # https://docs.python.org/3/library/struct.hatl#struct.calcsize
+    '''
+    ! tranforms our data from the network order (BE) to x64 (LE)
+    I means unsigned integer (4bytes: the buffer size we need)
 
-sockett.close() # Close the socket
+    (the combination of both "!I" is equivalent to ntohs in C)
+    '''
 
-'''
-! to tranform from the network order
-I meaning unsigned integer
-(equivalent to ntohs in C)
-'''
-time1970 = struct.unpack("!I", recv_data)[0] # https://docs.python.org/3/library/struct.html#struct.calcsize
+    time_since_1970 -= time_delta
+    actual_time = gmtime(time_since_1970) # easier to yank the desired data and format it
+    print(actual_time) # We shall format this data below
 
-time1970 -= time_delta
-actual_time = time.gmtime(time1970)
+def format_time(actual_time):
+    at = actual_time
+    s = str("Current time: " + get_week_day(at[6]) + ", " + str(at[0]) + "-" + str(at[1]) + "-" + str(at[2]) + ", " + str(at[3]) + ":" + str(at[4]) + ":" + str(at[5]))
+    return s
 
-print(actual_time)
-print(recv_data)
+def main():
+    get_current_time()
 
-
-
-'''
-[(<AddressFamily.AF_INET: 2>, <SocketKind.SOCK_DGRAM: 2>, 17, '', ('93.184.216.34', 80)),(<AddressFamily.AF_INET: 2>, <SocketKind.SOCK_STREAM: 1>, 6, '', ('93.184.216.34', 80)),(<AddressFamily.AF_INET6: 30>, <SocketKind.SOCK_DGRAM: 2>, 17, '', ('2606:2800:220:1:248:1893:25c8:1946', 80, 0, 0)),(<AddressFamily.AF_INET6: 30>, <SocketKind.SOCK_STREAM: 1>, 6, '', ('2606:2800:220:1:248:1893:25c8:1946', 80, 0, 0))]
-
-'''
+if __name__ == "__main__":
+    main()
