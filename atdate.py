ec5062ea740070b193fc67fdd3099b8670ae32cf
rodrigodelama <rodrigo.delama@icloud.com>
2022-09-27 22:37:51 +0200

    added debugging parameters
    slight cleanup

--- atdate.py
+++ atdate.py
@@ -18,16 +18,15 @@ time_delta = 2208988800 - 3600*2 # GMT - 2 hours for CET
 #TCP = "TCP"
 #UDP = "UDP"
 
-P = "-p"
 
-M = "-m"
-CU = "cu"
-CT = "ct"
-D = "-d"
-
-TIME_SERVER = "-s"
-
+HOSTNAME = "-s"
+MODE = "-m"
+UDP = "cu"
+TCP = "ct"
+TIME_SERVER = "s"
+PORT = "-p"
 DEFAULT_PORT = 37
+DEBUG = "-d"
 
 # Server constants
 SERV_BUFSIZE = 1024
@@ -42,29 +41,41 @@ def get_current_time(target, mode, port, debug_trigger):
     server = getaddrinfo(target, port)[0][-1] # 2D 
 
     # Test print input address data:
-    # (Format is: ('IP', PORT_NO) )
-    # print(server)
+    if debug_trigger == 1:
+        # (Format is: ('IP', PORT_NO) )
+        print(server)
 
-    if (mode == CU):
+    if (mode == UDP):
+        if debug_trigger == 1:
+            print("Attempting to open UDP socket")
         # We will create the socket w/ SOCK_DGRAM - UDP sends datagrams
         clientSocket = socket(AF_INET, SOCK_DGRAM)
-    elif(mode == CT):
+    elif(mode == TCP):
+        if debug_trigger == 1:
+            print("Attempting to open TCP socket")
         # We will create the socket w/ SOCK_STREAM - TCP sends streams of bytes
         clientSocket = socket(AF_INET, SOCK_STREAM)
     
     try:
         clientSocket.connect(server)
+        if debug_trigger == 1:
+            print("Connected to TIME server!")
     except gaierror:
         print("Error")
 
     # If the connection succeeds, send the empty message (by the users choice: UDP/TCP)
     clientSocket.send(bytes(0))
+    if debug_trigger == 1:
+            print("Sent empty UDP/TCP message (0bytes)")
 
     # Revieve the 4byte (32bit) current time data
     recv_data = clientSocket.recv(BUFSIZE)
-    # print(recv_data) # FIXME: WHAT ENCODING IS THIS ??
+    if debug_trigger == 1:
+            print("RAW recieved data: ", recv_data) # TODO: WHAT ENCODING IS THIS ??
 
     clientSocket.close() # Close the socket
+    if debug_trigger == 1:
+            print("Socket closed")
 
     time_since_1970 = struct.unpack("!I", recv_data)[0] # https://docs.python.org/3/library/struct.html
     '''
@@ -76,8 +87,11 @@ def get_current_time(target, mode, port, debug_trigger):
 
     time_since_1970 -= time_delta
     actual_time = gmtime(time_since_1970) # easier to yank the desired data and format it
-    # print(actual_time) # We shall format this data below
+    if debug_trigger == 1:
+            print("Complete formatted time: ", actual_time) # We will format this data below
     print(format_time(actual_time))
+    if debug_trigger == 1:
+            print("Program success! Closing now...")
     exit(0) # End program after succesful TIME request
 
 def format_time(actual_time):
@@ -133,7 +147,7 @@ def month(month):
         m = 'Dec'
     return m
 
-def time_server(listening_port):
+def time_server(listening_port, debug_trigger):
     # TODO:
     print("TIME server running on port ", listening_port)
 
@@ -212,34 +226,34 @@ def main():
     #print("argv_str: ", argv_str)
 
     try:
-        if (sys.argv.index("-d")):
+        if (sys.argv.index(DEBUG)):
             debug_trigger = 1
     except ValueError:
         debug_trigger = 0
     try:
-        if (sys.argv.index("-s")): # -s means HOSTNAME
-            # print(sys.argv.index("-s"))
-            target = sys.argv[sys.argv.index("-s")+1]
-            # print(sys.argv[sys.argv.index("-s")+1])
+        if (sys.argv.index(HOSTNAME)): # -s means HOSTNAME
+            # print(sys.argv.index(HOSTNAME))
+            target = sys.argv[sys.argv.index(HOSTNAME)+1]
+            # print(sys.argv[sys.argv.index(HOSTNAME)+1])
     except ValueError:
         try:
-            if (sys.argv.index("-m")):
+            if (sys.argv.index(MODE)):
                 print("Error: You must at least enter a HOSTNAME to run with default settings, as a client making a UDP request")
                 sys.exit(1)
         except ValueError:
-            if not (sys.argv.index("cu") or sys.argv.index("ct")):
+            if not (sys.argv.index(UDP) or sys.argv.index(TCP)):
                 print("Error: You must select SERVER mode (-m s) if you do not input a hostname")
                 sys.exit(1)
 
     try:
-        if (sys.argv.index("-m")):
-            mode = sys.argv[sys.argv.index("-m")+1]
+        if (sys.argv.index(MODE)):
+            mode = sys.argv[sys.argv.index(MODE)+1]
     except ValueError:
-        mode = CU # Default: UDP client
+        mode = UDP # Default: UDP client
 
     try:
-        if (sys.argv.index("-p")):
-            port = sys.argv[sys.argv.index("-p")+1]
+        if (sys.argv.index(PORT)):
+            port = sys.argv[sys.argv.index(PORT)+1]
     except ValueError: 
             port = 37 # Defaults: UDP client
 
@@ -248,9 +262,13 @@ def main():
         print("Target: ", target)
         print("Mode: ", mode)
 
-    get_current_time(target, mode, 37, debug_trigger)
+    if mode == UDP or mode == TCP:
+        get_current_time(target, mode, DEFAULT_PORT, debug_trigger)
+    elif mode == TIME_SERVER:
+        time_server(port, debug_trigger)
 
 '''
+OLD MANUAL STARTUP
     if (len(sys.argv) == 3):
         target = sys.argv[1]
         mode = sys.argv[2] # TEMPORARY
